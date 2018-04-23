7b8b683bd5f6dc46721fc1961017c370f4b38f16
Charles Sousa <charles@totalseg.bel>
2018-04-23 10:33:04 -0300

    commit modificado2

--- app.js
+++ app.js
@@ -138,7 +138,7 @@ git.Clone("https://github.com/deividoliver/funcionarios", "../../teste")
 
 
 var repoDir = "./";
-console.log("commit modificado"); 
+console.log("commit modificado 2");
 var repository;
 
 // Open a repository that needs to be fetched and fast-forwarded
