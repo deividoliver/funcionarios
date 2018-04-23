df04e8cd8b482de4b9662569f1d732207a0e03f9
Charles Sousa <charles@totalseg.bel>
2018-04-23 10:12:27 -0300

    commit invertido

--- app.js
+++ app.js
@@ -138,7 +138,7 @@ git.Clone("https://github.com/deividoliver/funcionarios", "../../teste")
 
 
 var repoDir = "./";
-
+console.log("commit invertido");
 var repository;
 
 // Open a repository that needs to be fetched and fast-forwarded
--- update.js
+++ update.js
@@ -1,39 +1,32 @@
 'use strict'
 
-var Git = require("nodegit");
+var repoDir = "../../test";
 
-// Clone a given repository into the `./tmp` folder.
-Git.Clone("https://github.com/deividoliver/funcionarios.git", "./tmp")
-  // Look up this known commit.
+var repository;
+
+// Open a repository that needs to be fetched and fast-forwarded
+nodegit.Repository.open(path.resolve(__dirname, repoDir))
   .then(function(repo) {
-      console.log('chegou aqui');
-    // Use a known commit sha from this repository.
-    return repo.getCommit("59b20b8d5c6ff8d09518454d4dd8b7b30f095ab5");
-  })
-  // Look up a specific file within that commit.
-  .then(function(commit) {
-    return commit.getEntry("README.md");
-  })
-  // Get the blob contents from the file.
-  .then(function(entry) {
-    // Patch the blob to contain a reference to the entry.
-    return entry.getBlob().then(function(blob) {
-      blob.entry = entry;
-      return blob;
+    repository = repo;
+
+    return repository.fetchAll({
+      callbacks: {
+        credentials: function(url, userName) {
+          return nodegit.Cred.sshKeyFromAgent(userName);
+        },
+        certificateCheck: function() {
+          return 1;
+        }
+      }
     });
   })
-  // Display information about the blob.
-  .then(function(blob) {
-    // Show the path, sha, and filesize in bytes.
-    console.log(blob.entry.path() + blob.entry.sha() + blob.rawsize() + "b");
-
-    
-    // Show a spacer.
-    console.log(Array(72).join("=") + "\n\n");
-
-    // Show the entire file.
-    console.log(String(blob));
+  // Now that we're finished fetching, go ahead and merge our local branch
+  // with the new one
+  .then(function() {
+    return repository.mergeBranches("master", "origin/master");
   })
-  .catch(function(err) { console.log(err); });
-
+  .done(function() {
+    console.log("Done!");
+  });
+  
   module.exports = Git;
